bcfe94d285a49b80f9c079729acb3ec572adb0c6
NguyenThanhThinh <thanhthinhcntt@gmail.com>
2017-05-19 12:58:54 +0700

    add repo

---
+++ QuanLyNhanSu/QuanLyNhanSu.Utilities/Extenstions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Data.Entity;
+
+namespace QuanLyNhanSu.Utilities
+{
+    public static class Extenstions
+    {
+        public static IQueryable<T> Including<T>(this IQueryable<T> self, params Expression<Func<T, object>>[] includeProperties) where T : class
+        {
+            return includeProperties.Aggregate(self, (current, includeProperty) => current.Include(includeProperty));
+        }
+    }
+}
