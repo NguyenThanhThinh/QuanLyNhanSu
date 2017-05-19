bab1f24f2713e7f797fd8606dac78dcad4439ba3
NguyenThanhThinh <thanhthinhcntt@gmail.com>
2017-05-19 12:45:21 +0700

    add all

---
+++ QuanLyNhanSu/QuanLyNhanSu.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QuanLyNhanSu.Web.Startup))]
+namespace QuanLyNhanSu.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
